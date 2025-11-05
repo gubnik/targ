27496d5751707831fb953f269734a4d448e89270
gubnik <danger8217.nik@gmail.com>
2025-11-05 12:38:33 +0300

    Add error formatter

--- include/targ_arg.hpp
+++ include/targ_arg.hpp
@@ -3,6 +3,8 @@
 #include "targ_hash_literal.hpp"
 #include "targ_type_traits.hpp"
 #include <expected>
+#include <format>
+#include <string_view>
 
 namespace ngg::targ
 {
@@ -15,6 +17,24 @@ enum class arg_error
     no_such_handler,
     value_out_of_range,
 };
+
+constexpr auto arg_error_to_string (arg_error err) noexcept -> std::string_view
+{
+    switch (err)
+    {
+        using enum arg_error;
+    case missing:
+        return "no value nor default";
+    case no_value_provided:
+        return "no value nor default";
+    case parser_error:
+        return "cannot parse provided value";
+    case no_such_handler:
+        return "no known way to handle the value";
+    case value_out_of_range:
+        return "value out of range";
+    }
+}
 
 template <is_arg_type Arg>
 using parsed_arg = std::expected<typename Arg::value_type, arg_error>;
@@ -36,3 +56,32 @@ template <hash_literal... args> struct arg
 };
 
 } // namespace ngg::targ
+
+template <typename CharT> struct std::formatter<ngg::targ::arg_error, CharT>
+{
+    constexpr auto parse (std::basic_format_parse_context<CharT> &ctx)
+        -> decltype(ctx.begin())
+    {
+        auto it = ctx.begin();
+        if (it != ctx.end() && *it != static_cast<CharT>('}'))
+        {
+            throw std::format_error("invalid format spec for Color");
+        }
+        return it;
+    }
+
+    template <typename FormatContext>
+    auto format (const ngg::targ::arg_error &c, FormatContext &ctx)
+        -> decltype(ctx.out())
+    {
+        if (auto name = ngg::targ::arg_error_to_string(c); !name.empty())
+        {
+            return std::format_to(
+                ctx.out(), "{}",
+                std::basic_string_view<CharT>(
+                    reinterpret_cast<const CharT *>(name.data()), name.size()));
+        }
+        using U = std::underlying_type_t<ngg::targ::arg_error>;
+        return std::format_to(ctx.out(), "{}", static_cast<U>(c));
+    }
+};
